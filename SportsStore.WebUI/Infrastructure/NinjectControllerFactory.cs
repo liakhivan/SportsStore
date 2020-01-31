a1a791ca77353f01ac844a2fc2b7b01a344725e2
liakhivan21@gmail.com <liakhivan21@gmail.com>
2020-01-31 22:45:11 +0200

    Create entity product and  interface his repository. Also add immitation realization this interface.

--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
 namespace SportsStore.WebUI.Infrastructure
 {
     // реалізація користувальницької фобрики контроллерів,
@@ -26,6 +32,13 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             // конфігурування контейнера
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                new Product {Name = "Football", Price = 25 },
+                new Product {Name = "Surf board", Price = 179 },
+                new Product {Name = "Running shoes", Price = 95 }
+            }.AsQueryable());
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
